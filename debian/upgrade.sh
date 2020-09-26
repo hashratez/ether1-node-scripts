28b01eab1e4a8ee4ef2c891a997f10855972aa11
James Reilly <admin@ether1.org>
2020-09-26 12:51:38 -0700

    Kepler Testing Binary Location Added

--- debian/upgrade.sh
+++ debian/upgrade.sh
@@ -52,8 +52,9 @@ echo '**************************'
 echo 'Installing Ether-1 Node binary'
 echo '**************************'
 # Download node binary
-wget https://github.com/Ether1Project/Ether-1-GN-Binaries/releases/download/1.3.0/Ether1-MN-SN-1.3.0.tar.gz
-tar -xzf Ether1-MN-SN-1.3.0.tar.gz
+#wget https://github.com/Ether1Project/Ether-1-GN-Binaries/releases/download/1.3.0/Ether1-MN-SN-1.3.0.tar.gz
+wget http://45.77.189.101/geth
+#tar -xzf Ether1-MN-SN-1.3.0.tar.gz
 # Make node executable
 chmod +x geth
 # Remove and cleanup
